445f8c6fc983a6f1ebc36a29cda049b471df8590
demtehai <demtehai@gmail.com>
2025-04-02 19:39:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import os
 import json
 from flask import Flask, jsonify
+from flask_cors import CORS
 from datetime import datetime, timedelta
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 # Flask app
 app = Flask(__name__)
+CORS(app)  # <- включаем CORS
 
 # Папка Google Drive
 FOLDER_ID = "1ji1tWY-qDFbSjzV7G6POvmS4NNJRHjf7"
 
-# Загружаем JSON из переменной среды
-credentials_info = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
+# Креды из ENV
+credentials_info = json.loads(os.environ["GOOGLE_CREDENTIALS"])
 credentials = service_account.Credentials.from_service_account_info(credentials_info)
 drive_service = build("drive", "v3", credentials=credentials)
 
@@ -31,7 +33,7 @@ def get_stories():
 
 @app.route("/")
 def index():
-    return "✅ GDrive Stories API is working."
+    return "✅ GDrive Stories API is running."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
